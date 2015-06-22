599c0a2965767ca173dc11f653cf189fe33abafd
Daniel Maatari Okouya <maatariokouya@gmail.com>
2015-06-21 22:41:52 -0400

    Added an Operational Cache with a PrefLabel Resolution part2

---
+++ src/main/scala/org/iadb/poolpartyconnector/thesauruscaching/JsonProtocolSpecification.scala
@@ -0,0 +1,20 @@
+package org.iadb.poolpartyconnector.thesauruscaching
+
+import spray.json.DefaultJsonProtocol
+
+/**
+ * Created by Daniel Maatari Okouya on 6/21/15.
+ */
+object JsonProtocolSpecification {
+
+
+  case class Concept(prefLabel: String, uri: String)
+
+
+  object JsonProtocol extends DefaultJsonProtocol {
+
+    implicit val ConceptFormat  = jsonFormat2(Concept)
+
+  }
+
+}
--- src/main/scala/org/iadb/poolpartyconnector/thesauruscaching/ThesaurusCacheService.scala
+++ src/main/scala/org/iadb/poolpartyconnector/thesauruscaching/ThesaurusCacheService.scala
@@ -2,6 +2,13 @@ package org.iadb.poolpartyconnector.thesauruscaching
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
+import spray.json._
+import JsonProtocolSpecification.Concept
+import JsonProtocolSpecification.JsonProtocol._
+
+import spray.client.pipelining._
+import spray.http.{HttpResponse, BasicHttpCredentials}
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 /**
@@ -13,6 +20,9 @@ import scala.concurrent.duration._
  */
 trait ThesaurusCacheService {
 
+  def getPrefLabelforConcept(uri: String, lang:String = "en") : String
+
+
   def isSchemeInCache(scheme: Option[String]): Boolean
 }
 
@@ -20,7 +30,7 @@ trait ThesaurusCacheService {
  * An Implementation of the CacheService for the PoolParty Thesaurus Server
  *
  */
-case class ThesaurusCacheServicePoolPartyImpl(name: String, actorSystem: ActorSystem) extends  ThesaurusCacheService {
+case class ThesaurusCacheServicePoolPartyImpl(name: String = "ThesaurusCacheServicePoolPartyImpl", actorSystem: ActorSystem) extends  ThesaurusCacheService {
 
   implicit private val requestTimeout = Timeout(800 seconds)
   implicit private val system         = actorSystem
@@ -49,6 +59,58 @@ case class ThesaurusCacheServicePoolPartyImpl(name: String, actorSystem: ActorSy
       case _ => false
 
 
+    }
+
+  }
+
+
+  /**
+   *
+   * @param uri
+   * @param lang
+   * @return
+   */
+  override def getPrefLabelforConcept(uri: String, lang:String = "en"): String = {
+
+    import system.dispatcher
+
+
+    val pipeline      = addCredentials(BasicHttpCredentials("superadmin", "poolparty")) ~> sendReceive
+
+    val request       = Get(s"http://127.0.0.1:8086/PoolParty/api/thesaurus/1DCDFC5D-3876-0001-EEE6-BC9C1B8016CF/concept?concept=$uri&language=$lang")
+
+    val res           = pipeline(request)
+
+    getConceptFromFutureHttpResponse(res) match {
+      case Some(e) => e.prefLabel
+      case _ => ""
+    }
+  }
+
+
+  /**
+   *  Await for the future Http response to arrive
+   *  Get the response or the error if any
+   *  Return the response as a ConceptResult Object
+   *  The Object is empty if there was an error
+   *
+   * @param res
+   * @return
+   */
+  private def getConceptFromFutureHttpResponse(res: Future[HttpResponse]): Option[Concept] = {
+
+    try {
+
+      val reponse = Await.result(res, Duration.Inf)
+
+      println(reponse.entity.data.asString)
+
+      Some(reponse.entity.data.asString.parseJson.convertTo[Concept])
+
+    }catch {
+
+      case e:Throwable => None
+      //TODO Add the possible throwable here: those of await result and log the error
     }
 
   }
