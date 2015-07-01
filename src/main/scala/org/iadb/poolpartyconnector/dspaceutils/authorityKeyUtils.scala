a7ea5c570528531fea35abec627d1d17916250c5
Daniel Maatari Okouya <maatariokouya@gmail.com>
2015-07-01 15:02:32 -0400

    Utils for Dspace to deal with authorityKey

---
+++ src/main/scala/org/iadb/poolpartyconnector/dspaceutils/authorityKeyUtils.scala
@@ -0,0 +1,17 @@
+package org.iadb.poolpartyconnector.dspaceutils
+
+/**
+ * Created by Daniel Maatari Okouya on 6/22/15.
+ */
+
+case class AuthorityandKey(auth: String, key:String)
+
+object AuthorityKeyUtils {
+
+  //TODO Test and handle corner case to return ("", "")
+  def splitAuthrotiyandKey(authkey: String): AuthorityandKey = {
+    val authkeyTuple = authkey.splitAt(authkey.indexOf(':', 5))
+    AuthorityandKey(authkeyTuple._1, authkeyTuple._2.substring(1))
+  }
+
+}
